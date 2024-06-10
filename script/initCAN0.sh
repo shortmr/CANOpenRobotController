3323e486772dd861dc0dfa786d7c416b5e582204
shortmr <matthewshort2025@u.northwestern.edu>
2024-06-10 16:42:28 -0500

    add reset CAN

--- script/initCAN0.sh
+++ script/initCAN0.sh
@@ -4,6 +4,7 @@
 #assumes proper configuration on CAN0 (see https://stackoverflow.com/questions/62207737/beaglebone-ai-how-to-setup-can-bus)
 
 echo "Enabling CAN0"
+sudo ip link set can0 down
 sudo ip link set can0 up type can bitrate 1000000
 sudo ifconfig can0 up
 #sudo ifconfig can0 txqueuelen 1000

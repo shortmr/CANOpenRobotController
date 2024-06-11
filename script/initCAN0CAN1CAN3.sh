165eee69a4ecca71f0d75655dbf520f2d057f829
shortmr <matthewshort2025@u.northwestern.edu>
2024-06-11 17:48:21 -0500

    added extra launch and init for 3rd M1

---
+++ script/initCAN0CAN1CAN3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#initialisation of CAN interfaces on BB
+
+echo "Enabling CAN0"
+sudo ip link set can0 down
+sudo ip link set can0 up type can bitrate 1000000
+sudo ifconfig can0 up
+
+echo "Enabling CAN1"
+sudo ip link set can1 down
+sudo ip link set can1 up type can bitrate 1000000
+sudo ifconfig can1 up
+
+echo "Enabling CAN3"
+sudo ip link set can3 down
+sudo ip link set can3 up type can bitrate 1000000
+sudo ifconfig can3 up
+
+#echo "can0 up. Dumping (ctrl+c to close):"
+#candump -c -t z can0,080~111111 #Filter out 080 sync messages
